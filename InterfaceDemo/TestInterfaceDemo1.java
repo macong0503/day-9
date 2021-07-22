f63f230e31eb13dba0fd270e379803a8d3e02bc1
macong0503 <1512346520@qq.com>
2021-07-22 17:14:30 +0800

    Create TestInterfaceDemo1.java

---
+++ InterfaceDemo/TestInterfaceDemo1.java
@@ -0,0 +1,10 @@
+package com.hqyj.mc.InterfaceDemo;
+
+public class TestInterfaceDemo1 {
+    public static void main(String[] args) {
+        InterfaceDemo1Impl i = new InterfaceDemo1Impl();
+        i.aMethod();
+    }
+
+
+}
