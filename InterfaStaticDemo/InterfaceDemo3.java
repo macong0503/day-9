87df43c55cb534062f442bab333d7b8396ca54d1
macong0503 <1512346520@qq.com>
2021-07-22 17:14:45 +0800

    Create InterfaceDemo3.java

---
+++ InterfaStaticDemo/InterfaceDemo3.java
@@ -0,0 +1,8 @@
+package com.hqyj.mc.InterfaStaticDemo;
+
+public interface InterfaceDemo3 {
+    //静态方法  权限修饰符只能用publi
+    public static void a(){
+        System.out.println("这是接口中的静态方法");
+    }
+}
