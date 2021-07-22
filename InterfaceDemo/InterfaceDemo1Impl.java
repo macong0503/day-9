320272ddc0bc568e8f8e5043e9e06e33b3ac20da
macong0503 <1512346520@qq.com>
2021-07-22 17:14:28 +0800

    Create InterfaceDemo1Impl.java

---
+++ InterfaceDemo/InterfaceDemo1Impl.java
@@ -0,0 +1,8 @@
+package com.hqyj.mc.InterfaceDemo;
+//格式public class 类名 implements 接口名
+public class InterfaceDemo1Impl implements InterfaceDemo1 {
+    @Override
+    public void aMethod() {
+        System.out.println("这是一个接口实现类");
+    }
+}
