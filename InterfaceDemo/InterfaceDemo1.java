1d203828574366284d0370d5b67da8e0ad3bc25b
macong0503 <1512346520@qq.com>
2021-07-22 17:14:25 +0800

    Create InterfaceDemo1.java

---
+++ InterfaceDemo/InterfaceDemo1.java
@@ -0,0 +1,15 @@
+package com.hqyj.mc.InterfaceDemo;
+//格式public interface 接口名
+public interface InterfaceDemo1 {
+//接口中没有构造方法
+//    public void amethod(){
+//
+//    }
+
+    //抽象方法  接口中的抽象方法，必须是public abstr修饰的，且可以省略不写
+    public abstract void aMethod();
+  /*  public void a();
+    abstract void b();//不写也是public修饰符，不是默认修饰符
+    void c();*/
+
+}
