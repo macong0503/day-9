f93edbc3be9870e6aaa28fe2c2b002d75a91ad4d
macong0503 <1512346520@qq.com>
2021-07-22 17:15:11 +0800

    Create ClazzC.java

---
+++ InterfaceMoreIpml/ClazzC.java
@@ -0,0 +1,12 @@
+package com.hqyj.mc.InterfaceMoreIpml;
+
+public abstract class ClazzC {
+
+public abstract void methodD();
+public abstract void methodA();
+public abstract void methodB();
+public abstract void methodC();
+    public  void Default1(){
+        System.out.println("这是ClazzC类中的默认方法");
+    }
+}
