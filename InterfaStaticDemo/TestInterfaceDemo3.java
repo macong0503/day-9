83d52d35186a3b9f06a4afdf91d120dbc5ff9f0b
macong0503 <1512346520@qq.com>
2021-07-22 17:14:48 +0800

    Create TestInterfaceDemo3.java

---
+++ InterfaStaticDemo/TestInterfaceDemo3.java
@@ -0,0 +1,11 @@
+package com.hqyj.mc.InterfaStaticDemo;
+
+import static com.hqyj.mc.InterfaStaticDemo.InterfaceDemo3.a;
+
+public class TestInterfaceDemo3 {
+    public static void main(String[] args) {
+    a();
+
+    a();
+    }
+}
