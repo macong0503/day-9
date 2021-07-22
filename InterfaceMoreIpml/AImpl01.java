7e73de924d9b55bfa89a476b4c13e88e49854ba6
macong0503 <1512346520@qq.com>
2021-07-22 17:15:16 +0800

    Create AImpl01.java

---
+++ InterfaceMoreIpml/AImpl01.java
@@ -0,0 +1,28 @@
+package com.hqyj.mc.InterfaceMoreIpml;
+
+public class AImpl01 extends ClazzC implements A ,B{
+    @Override
+    public void methodA() {
+
+    }
+
+    @Override
+    public void methodB() {
+
+    }
+
+    @Override
+    public void methodD() {
+
+    }
+
+    @Override
+    public void methodC() {
+
+    }
+
+    @Override
+    public void Default() {
+        System.out.println("这是实现类01中的默认方法");
+    }
+}
