44b00fec52dabbb4563eac460800a89307140dd4
macong0503 <1512346520@qq.com>
2021-07-22 17:14:42 +0800

    Create TestInterfaceDemo2.java

---
+++ Interfacerdefault/TestInterfaceDemo2.java
@@ -0,0 +1,9 @@
+package com.hqyj.mc.Interfacerdefault;
+
+public class TestInterfaceDemo2 {
+    public static void main(String[] args) {
+
+        InterfaceDemo2Impl i = new InterfaceDemo2Impl();
+    i.a();
+    }
+}
