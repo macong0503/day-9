74099800bcf2f8cb1a3b7dfca6c54e410e1d8541
macong0503 <1512346520@qq.com>
2021-07-22 17:15:08 +0800

    Create Test.java

---
+++ InterfaceMoreIpml/Test.java
@@ -0,0 +1,9 @@
+package com.hqyj.mc.InterfaceMoreIpml;
+
+public class Test {
+    public static void main(String[] args) {
+        AImpl01 a = new AImpl01();
+        a.Default();//这是实现类01中的默认方法
+        a.Default1();//这是ClazzC类中的默认方法
+    }
+}
