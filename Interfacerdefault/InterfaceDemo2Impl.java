4481059187a5d54fdf10a7982578c0b8e913ffab
macong0503 <1512346520@qq.com>
2021-07-22 17:14:39 +0800

    Create InterfaceDemo2Impl.java

---
+++ Interfacerdefault/InterfaceDemo2Impl.java
@@ -0,0 +1,8 @@
+package com.hqyj.mc.Interfacerdefault;
+
+public class InterfaceDemo2Impl implements InterfaceDemo2{
+    @Override
+    public void a() {
+        System.out.println("这是实现类");
+    }
+}
